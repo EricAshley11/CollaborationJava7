aae8f3d760c9babc623fcecedbaeba4cd47ea571
Michael Vokes <vokesm@mail.gvsu.edu>
2012-10-09 15:20:31 -0400

    Added methods for milestones

--- src/collaboration/Project.java
+++ src/collaboration/Project.java
@@ -14,6 +14,7 @@ import javax.persistence.*;
 public class Project implements Serializable{
     private Collection<Team> teams;
     private Collection<UserStory> userStories;
+    private Collection<Milestone> milestones;
     private String name;
     //Calendar is not yet implemented private GCal calendar;   
     Project(String name){
@@ -45,6 +46,7 @@ public class Project implements Serializable{
         }
         return false;
     }
+    
     boolean removeUserStory(UserStory userStory) {
         if(userStories.remove(userStory)){
             userStory.removeProject(this);
@@ -57,6 +59,21 @@ public class Project implements Serializable{
         if(!userStories.contains(userStory)){
             userStories.add(userStory);
             userStory.changeProject(this);
+            return true;
+        }
+        return false;
+    }
+    
+    boolean createMilestone(Milestone milestone) {
+        if (!milestones.contains(milestone)) {
+            milestones.add(milestone);
+            return true;
+        }
+        return false;
+    }
+    
+    boolean removeMilestone(Milestone milestone) {
+        if (milestones.remove(milestone)) {
             return true;
         }
         return false;
