9e78f4268466424ee2f0891a8ce995f1d76b8237
Eric Munson <munsone@mail.gvsu.edu>
2012-10-30 17:22:13 -0400

    Adding Android

---
+++ CollabAndroid/gen/com/src/collabandroid/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.src.collabandroid;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ CollabAndroid/src/com/src/collabandroid/VideosActivity.java
@@ -0,0 +1,13 @@
+package com.src.collabandroid;
+
+import com.src.collabandroid.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class VideosActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.videos_layout);
+    }
+}
