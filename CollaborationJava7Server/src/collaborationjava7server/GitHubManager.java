740d38610197e8e33669d025b55792821098edf3
Zach Burnside <akie9001@gmail.com>
2012-12-04 18:07:14 -0500

    Created methods for GitHubmanager for issues and project tracking

--- CollaborationJava7Server/src/collaborationjava7server/GitHubManager.java
+++ CollaborationJava7Server/src/collaborationjava7server/GitHubManager.java
@@ -3,7 +3,9 @@
  * and open the template in the editor.
  */
 package collaborationjava7server;
+
 import java.io.IOException;
+import java.util.List;
 import org.eclipse.egit.github.core.*;
 import org.eclipse.egit.github.core.client.*;
 import org.eclipse.egit.github.core.event.*;
@@ -15,24 +17,56 @@ import org.eclipse.egit.github.core.util.*;
  * @author burnsidz
  */
 public class GitHubManager {
+
     String userName;
     String repoName;
 
-    
-    private void commit(){
-        
+    private void reportIssue() {
     }
-    private void pull(){
-        
+
+    private void getBranches(String ownerUsername, String reponame) throws IOException {
+        RepositoryService rServ = new RepositoryService();
+        RepositoryId rID = new RepositoryId(ownerUsername, reponame);
+        List<RepositoryBranch> l = rServ.getBranches(rID);
+        for (int i = 0; i < l.size(); i++) {
+            System.out.println(rServ.getBranches(rID).get(i).getName());
+        }
     }
-    private void push(){
-        
+
+    private void getContributors(String ownerUsername, String reponame) throws IOException {
+        RepositoryService rServ = new RepositoryService();
+        RepositoryId rID = new RepositoryId(ownerUsername, reponame);
+        List<Contributor> l = rServ.getContributors(rID, false);
+        for (int i = 0; i < l.size(); i++) {
+            rServ.getContributors(rID, false).get(i).getLogin();
+        }
+
     }
-    private void reportIssue(){
-        
+
+    private void getIssues(String ownerUsername, String reponame) throws IOException {
+        IssueService iServ = new IssueService();
+        RepositoryId rID = new RepositoryId(ownerUsername, reponame);
+        List<Issue> l = iServ.getIssues(rID, null);
+        for (int i = 0; i < l.size(); i++) {
+            Issue issue = iServ.getIssues(rID, null).get(i);
+            issue.getTitle();
+            issue.getBody();
+            issue.getUser().getLogin();
+        }
     }
-    public static void main(String[] args) throws IOException  {
+
+    private void createIssue(String username, String password, String reponame, String title, String body) throws IOException {
         GitHubClient client = new GitHubClient();
-        client.setCredentials("akie311", "bd349557");
+        client.setCredentials(username, password);
+        IssueService iServ = new IssueService(client);
+        RepositoryId rID = new RepositoryId(username, reponame);
+        Issue issue = new Issue();
+        issue.setTitle(title);
+        issue.setBody(body);
+        iServ.createIssue(rID, issue);
+    }
+
+    public static void main(String[] args) throws IOException {
+        
     }
 }
